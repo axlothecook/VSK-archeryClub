2dcac8444a4b9e739eeed83f07ac5bff8a384a65
axlothecook <alliston.ruby123@gmail.com>
2024-05-14 20:13:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//news carousel
+const newsposts = [
+    {
+        id: 1,
+        location: 'Europsko dvoransko prvenstvo',
+        date: '19.-24.2.2024.',
+        image: 'images/test1.jpg',
+        description: 
+        'Četiri plasmana u top 4 i dvije medalje s europskog dvoranskog prvenstva za VSK!',
+    },
+    {
+        id: 2,
+        location: 'Nimes, Francuska ',
+        date: '19.-21.1.2024.',
+        image: 'images/test3.jpg',
+        description: 
+        'Leo Sulik pobjednik finala juniorskog svjetskog kupa, David Lazar četvrti!',
+    },
+    {
+        id: 1,
+        location: 'Europsko dvoransko prvenstvo',
+        date: '8.-10.12.2023.',
+        image: 'images/test4.jpg',
+        description: 
+        'Alen Remar treći na Berlinu Openu!🥉',
+    }
+]
+
+
+//select items
+let btn = document.querySelector()
