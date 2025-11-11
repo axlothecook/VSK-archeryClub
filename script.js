f0d1dc7568cdbbf2d1d1acf0898607f3bec9c066
axlothecook <alliston.ruby123@gmail.com>
2025-11-12 00:25:07 +0100

    final changes

--- script.js
+++ script.js
@@ -1,31 +1,87 @@
-//news carousel
-const newsposts = [
-    {
-        id: 1,
-        location: 'Europsko dvoransko prvenstvo',
-        date: '19.-24.2.2024.',
-        image: 'images/test1.jpg',
-        description: 
-        'Četiri plasmana u top 4 i dvije medalje s europskog dvoranskog prvenstva za VSK!',
-    },
-    {
-        id: 2,
-        location: 'Nimes, Francuska ',
-        date: '19.-21.1.2024.',
-        image: 'images/test3.jpg',
-        description: 
-        'Leo Sulik pobjednik finala juniorskog svjetskog kupa, David Lazar četvrti!',
-    },
-    {
-        id: 1,
-        location: 'Europsko dvoransko prvenstvo',
-        date: '8.-10.12.2023.',
-        image: 'images/test4.jpg',
-        description: 
-        'Alen Remar treći na Berlinu Openu!🥉',
-    }
-]
+//code for image carousel
+let count = 1;
+let slidesContainer = document.querySelector('.carousel-frame');
 
+function increaseCount(){
+	let smt = count;
+	return (smt + 1 === 4) ? smt = 1 : smt + 1;
 
-//select items
-let btn = document.querySelector()
+};
+
+function decreaseCount(){
+	let smt = count;
+	return (smt - 1 === 0) ? smt = 3 : smt -= 1;
+
+};
+
+window.addEventListener('load', function (e) {
+	var currentImg = document.querySelector(`.image-div-${count}`);
+	currentImg.classList.add('show');
+
+	document.querySelector('.circle-1').classList.add('color');
+
+});
+
+var next = function (count) {
+	var currentImg = document.querySelector(`.image-div-${count}`);
+	if (!currentImg.classList.contains('show')) return;
+
+	var currentCircle = document.querySelector(`.circle-${count}`);
+	if (!currentCircle.classList.contains('color')) return;
+
+	var nextImg = document.querySelector(`.image-div-${increaseCount()}`);
+	if (nextImg.classList.contains('show')) return;
+
+	currentImg.classList.remove('show');
+	nextImg.classList.add('show');
+	
+	//for circle
+	var nextCircle = document.querySelector(`.circle-${increaseCount()}`);
+	if (nextCircle.classList.contains('color')) return;
+
+	currentCircle.classList.remove('color');
+	// slidesContainer.style.transition = 'transform 0.4s ease-in-out'; doesnt work
+	nextCircle.classList.add('color');
+
+};
+
+var previous = function (count) {
+	var currentImg = document.querySelector(`.image-div-${count}`);
+	if (!currentImg.classList.contains('show')) return;
+
+	var currentCircle = document.querySelector(`.circle-${count}`);
+	if (!currentCircle.classList.contains('color')) return;
+
+	var previousImg = document.querySelector(`.image-div-${decreaseCount()}`);
+	if (previousImg.classList.contains('show')) return;
+
+	currentImg.classList.remove('show');
+	previousImg.classList.add('show');
+
+	//for circle
+	var previousCircle = document.querySelector(`.circle-${decreaseCount()}`);
+	if (previousCircle.classList.contains('color')) return;
+
+	currentCircle.classList.remove('color');
+	previousCircle.classList.add('color');
+
+};
+
+var changeImage = function (event) {
+	if (!event.target.classList.contains('arrow')) return;
+
+	var arrow = event.target;
+	if (arrow.classList.contains('right-side-arrow')) {
+		next(count);
+		(count + 1 != 4) ? count += 1 : count = 1;
+	};
+
+	if (arrow.classList.contains('left-side-arrow')) {
+		previous(count);
+		(count - 1 != 0) ? count -= 1 : count = 3;
+	};
+}
+
+document.addEventListener('click', function (event) {
+	changeImage(event);
+}, false);
